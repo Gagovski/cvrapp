fb99eb8fa501d208e88edb1d5bfb3ac63abc4bf8
ian <ian_yin@gcoreinc.com>
2017-11-22 15:10:06 +0800

    [cvrapp] update FileItemList
    Change-Id: Ie31ff89999e2b4d0dc1e77c03544121ee5b6fc93

--- app/src/main/java/com/gc/cvrapp/activity/FileItemList.java
+++ app/src/main/java/com/gc/cvrapp/activity/FileItemList.java
@@ -3,22 +3,42 @@ package com.gc.cvrapp.activity;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * class for file item list
+ */
 public class FileItemList implements Serializable {
     private List<String> filelist;
     private String curFile;
 
+    /**
+     * get file list
+     * @return file list
+     */
     public List<String> getFilelist() {
         return filelist;
     }
 
+
+    /**
+     * set file list
+     * @param filelist file list
+     */
     public void setFilelist(List<String> filelist) {
         this.filelist = filelist;
     }
 
+    /**
+     * get current file item
+     * @return current file item name
+     */
     public String getCurFile() {
         return this.curFile;
     }
 
+    /**
+     * get current file item position
+     * @return current file item position
+     */
     public int getCurPosition() {
         int pos = 0;
         for (String filename : filelist) {
@@ -30,14 +50,27 @@ public class FileItemList implements Serializable {
         return 0;
     }
 
+    /**
+     * get specify file item name
+     * @param pos specify file item position
+     * @return specify file item name
+     */
     public String getFile(int pos) {
         return filelist.get(pos);
     }
 
+    /**
+     * set specify file item name
+     * @param curFile current file item position
+     */
     public void setCurFile(String curFile) {
         this.curFile = curFile;
     }
 
+    /**
+     * get file list item count
+     * @return total file items count
+     */
     public int getCount() {
         return filelist.size();
     }
